956bc5caa43bdd7ad0bd92c2848e81b50cba224e
fcesnek <filip.cesnek@gmail.com>
2019-12-31 02:15:44 +0100

    Added vue router and vuex, setup backend structure

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const config = require('./config/config');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+
+mongoose.connect(config.dbPath, {useNewUrlParser: true})
+  .then(() => console.log('Connected to the database...'))
+  .catch((err) => console.error('Could not connect to the database...', err))
+
+app.listen(config.port);
+console.log(`App started on port ${config.port}`);
+
+
